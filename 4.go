e9d8fa423d9149f8a75aa74a1b710601cb1c793e
wangqianwei <wangqianwei@360.cn>
2021-05-12 11:54:06 +0800

    update

--- 4.go
+++ 4.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // 4. 寻找两个正序数组的中位数
 // 难度 困难
 // https://leetcode-cn.com/problems/median-of-two-sorted-arrays/
@@ -8,12 +10,58 @@ package main
 
 func main() {
 
+	//var nums1, nums2 []int = []int{1, 3}, []int{2}
+	//var nums1, nums2 []int = []int{1, 2}, []int{3, 4}
+	//var nums1, nums2 []int = []int{0, 0}, []int{0, 0}
+	//var nums1, nums2 []int = []int{}, []int{1}
+	var nums1, nums2 []int = []int{2}, []int{}
+
+	r := findMedianSortedArrays(nums1, nums2)
+	fmt.Println(r)
+
 }
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 
-	var i []int = make([]int, len(nums1)+len(nums2))
-	// 同时一起遍历两个数组
+	var i, j, sum []int = nums1, nums2, []int{}
+	var ic, jc int = 0, 0
+	for c := 0; c < len(i)+len(j); c++ {
+		if ic != len(i) && jc != len(j) {
+			if i[ic] < j[jc] {
+				sum = append(sum, i[ic])
+				ic++
+			} else {
+				sum = append(sum, j[jc])
+				jc++
+			}
+		} else {
+			if ic != len(i) {
+				for {
+					if ic == len(i) {
+						break
+					}
+					sum = append(sum, i[ic])
+					ic++
+				}
+			}
+			if jc != len(j) {
+				for {
+					if jc == len(j) {
+						break
+					}
+					sum = append(sum, j[jc])
+					jc++
+				}
+			}
+		}
+	}
 
+	if len(sum)%2 == 0 {
+		mid := len(sum) / 2
+		return float64((sum[mid-1] + sum[mid])) / 2
+	} else {
+		mid := len(sum) / 2
+		return float64(sum[mid])
+	}
 	return 0
 }
---
+++ 5.go
@@ -0,0 +1,16 @@
+package main
+
+// 5. 最长回文子串
+// 难度 中等 
+// https://leetcode-cn.com/problems/longest-palindromic-substring/
+
+// 给你一个字符串 s，找到 s 中最长的回文子串。
+
+func main() {
+
+}
+
+func longestPalindrome(s string) string {
+
+	return ""
+}
